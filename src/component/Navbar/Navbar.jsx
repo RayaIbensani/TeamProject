13a78b5fb39b671da7b522f9e312431d057d154a
RayaIbensani <rayaibensani@gmail.com>
2022-10-25 14:12:50 +0800

    Fix problem

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './NavStyle.css'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 import { CheckLogin } from "../../helpers/checkLogin"
 import { useNavigate } from "react-router-dom"
 import Swal from "sweetalert2";
@@ -29,39 +29,49 @@ function NavBar() {
 
   return (
     <>
-      <Navbar className='navbarr navbar-dark' expand="lg">
-        <Container fluid>
-          <Navbar.Brand Link="#">NgiungTRVL</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="mx-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Nav.Link Link="/">{context.lang==='en' ? 'Home' : 'Beranda'}</Nav.Link>
-              <NavDropdown title={context.lang==='en' ? 'Destination' : 'Destinasi'} id="navbarScrollingDropdown">
-                <NavDropdown.Item Link="/sumbawa">Sumbawa Besar</NavDropdown.Item>
-                <NavDropdown.Item Link="/ksb">
-                  Sumbawa Barat
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link Link="/aboutus">
+      <nav className='navbar navbar-expand-lg'>
+        <div className="container-fluid">
+          <Link to="/" className='navbar-brand'>NgiungTRVL</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" style={{ color: "black" }}></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarScroll">
+      <ul className="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style={{bsscrollheight: "100px"}}>
+        <li className='nav-item'>
+              <Link to="/" className='nav-link active'>{context.lang==='en' ? 'Home' : 'Beranda'}</Link>
+              </li>
+              <li className='nav-item dropdown'> 
+              <div className=" nav-link dropdown-toggle" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {context.lang==='en' ? 'Destination' : 'Destinasi'}
+              </div>
+              <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                <li><Link to="/sumbawa" className="dropdown-item">Sumbawa Besar</Link></li>
+                <li><Link to="/ksb" className="dropdown-item">Sumbawa Barat</Link></li>
+                </ul>  
+              </li>
+
+              <li className='nav-item'>
+              <Link to="/aboutus" className='nav-link active'>
               {context.lang==='en' ? 'About' : 'Tentang'}
-              </Nav.Link>
+              </Link>
+              </li>
               {
                 isLogin ?
+                <li className='nav-item'>
                   <Nav.Link onClick={handleLogout}>{context.lang==='en' ? 'Logout' : 'Keluar'}</Nav.Link>
-                  :
-                  <Nav.Link Link="/login">
+             </li> :
+              <li className='nav-item'>
+                  <Link to="/login" className="nav-link active">
                     {context.lang==='en' ? 'Login' : 'Masuk'}
-                  </Nav.Link>
+                  </Link>
+                  </li>
               }
-            </Nav>
-            <Button variant="outline-light" onClick={() => context.onchangeLang(context.lang === "en" ? "id" : "en")}>EN / ID</Button>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+             </ul> 
+            
+            <button className="button" variant="outline-light" onClick={() => context.onchangeLang(context.lang === "en" ? "id" : "en")}>EN / ID</button>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
